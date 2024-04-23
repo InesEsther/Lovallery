2042517f81a5bfec8712d2a1fc35a3e830749795
Ines Leca <inescuchi2@gmail.com>
2024-04-23 11:51:15 +0200

    first commit

---
+++ Assets/_MAIN/Scripts/Core/DialogueContainer.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueContainer : MonoBehaviour
+{
+    public GameObject root;
+    public TMP_text nameText; 
+    public TMP_text dialogueText;
+}
