a690966b926229f2876080459799f9cd92e42140
carchiloan <carchiloan@gmail.com>
2024-05-30 00:29:11 +0200

    Edicion Audio pt 5 creo

--- Assets/_MAIN/Scripts/Audio/AudioManager.cs
+++ Assets/_MAIN/Scripts/Audio/AudioManager.cs
@@ -5,66 +5,75 @@ using UnityEngine.SceneManagement;
 
 public class AudioManager : MonoBehaviour
 {
-    public AudioClip bandaSonoraMenuEscena1;
-    public AudioClip bandaSonoraEscena3Degas1;
-    public AudioClip bandaSonoraGeneral;
+    public AudioClip MenuEscena1;
+    public AudioClip[] Escena3;
+    public AudioClip Degas1;
+    public AudioClip[] Escena1;
+    public AudioClip[] Escena2;
 
     private AudioSource _audioSource;
 
-    // Listas de nombres de escenas donde el sonido debería reproducirse
-    private readonly List<string> escenasMenuEscena1 = new List<string> { "Menu", "Escena1" };
-    private readonly List<string> escenasEscena3Degas1 = new List<string> { "Escena3", "Degas1" };
+    private Dictionary<string, AudioClip[]> Sounds = new Dictionary<string, AudioClip[]>();
 
     public static AudioManager Instance { get; private set; }
 
     void Awake()
     {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-            _audioSource = GetComponent<AudioSource>();
-
-            SceneManager.sceneLoaded += OnSceneLoaded;
-
-            // Comprobar la escena actual y reproducir el sonido adecuado
-            CheckCurrentScene(SceneManager.GetActiveScene());
-        }
-        else
+        if (Instance != null)
         {
             Destroy(gameObject);
+            return;
         }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        _audioSource = GetComponent<AudioSource>();
+
+        SceneManager.sceneLoaded += OnSceneLoaded;
+
+        // Añadir los clips de audio a las escenas en el diccionario
+        Sounds.Add("Menu", new AudioClip[] { MenuEscena1 });
+        Sounds.Add("Escena1", new AudioClip[] { MenuEscena1 });
+        Sounds.Add("Escena2", Escena2);
+        Sounds.Add("Escena3", Escena3);
+        Sounds.Add("Degas1", new AudioClip[] { Degas1 });
+        // Añadir los nuevos clips de audio para las nuevas escenas
+        
     }
 
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        CheckCurrentScene(scene);
-    }
+        string sceneName = scene.name;
+        if (!Sounds.ContainsKey(sceneName))
+        {
+            StopSound();
+            return;
+        }
 
-    private void CheckCurrentScene(Scene scene)
-    {
-        if (escenasMenuEscena1.Contains(scene.name))
+        AudioClip[] sceneSounds = Sounds[sceneName];
+        if (sceneSounds.Length == 0)
         {
-            PlaySound(bandaSonoraMenuEscena1);
+            StopSound();
+            return;
         }
-        else if (escenasEscena3Degas1.Contains(scene.name))
-        {
-            PlaySound(bandaSonoraEscena3Degas1);
-        }
-        else
-        {
-            PlaySound(bandaSonoraGeneral);
-        }
+
+        AudioClip randomSound = sceneSounds[Random.Range(0, sceneSounds.Length)];
+        PlaySound(randomSound);
     }
 
     private void PlaySound(AudioClip clip)
     {
-        if (_audioSource.clip != clip)
+        if (!_audioSource.isPlaying)
         {
             _audioSource.clip = clip;
             _audioSource.loop = true;
             _audioSource.Play();
         }
+    }
+
+    private void StopSound()
+    {
+        _audioSource.Stop();
     }
 
     private void OnDestroy()
