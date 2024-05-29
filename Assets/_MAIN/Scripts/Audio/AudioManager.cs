fcd2a37e39339619a5ce68103f6238d5dcbc125f
carchiloan <carchiloan@gmail.com>
2024-05-29 21:44:01 +0200

    Editando audio parte 2

--- Assets/_MAIN/Scripts/Audio/AudioManager.cs
+++ Assets/_MAIN/Scripts/Audio/AudioManager.cs
@@ -1,29 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AudioManager : MonoBehaviour
 {
     public AudioClip bandaSonora;
-
     public AudioClip pop;
 
-    AudioSource _audioSource;
+    private AudioSource _audioSource;
 
-    // Start is called before the first frame update
-    void Start()
+    public static AudioManager Instance { get; private set; }
+
+    // Lista de nombres de escenas donde el sonido debería reproducirse
+    private readonly List<string> escenasPermitidas = new List<string> { "Menu", "Escena1" };
+
+    void Awake()
     {
-        _audioSource = this.GetComponent<AudioSource>();
-        _audioSource.clip = bandaSonora;
-        _audioSource.loop = true;
-        //_audioSource.play( );
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+
+            SceneManager.sceneLoaded += OnSceneLoaded;
+
+            _audioSource = GetComponent<AudioSource>();
+            _audioSource.clip = bandaSonora;
+            _audioSource.loop = true;
+            _audioSource.Play();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    //HACER UN SINGLETON, UN 
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (!escenasPermitidas.Contains(scene.name))
+        {
+            _audioSource.Stop();
+        }
+        else if (!_audioSource.isPlaying)
+        {
+            _audioSource.Play();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Aquí puedes agregar la lógica para el update si es necesario
     }
 }
