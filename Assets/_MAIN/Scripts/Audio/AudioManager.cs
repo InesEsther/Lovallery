7953a52a9c7da994c2aabaf47754a643ebe80c94
carchiloan <carchiloan@gmail.com>
2024-05-31 13:53:46 +0200

    A tomar viento el Audio pt1

--- Assets/_MAIN/Scripts/Audio/AudioManager.cs
+++ Assets/_MAIN/Scripts/Audio/AudioManager.cs
@@ -1,88 +1,71 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {
-    public AudioClip MenuEscena1;
-    public AudioClip[] Escena3;
-    public AudioClip Degas1;
-    public AudioClip[] Escena1;
-    public AudioClip[] Escena2;
-    public AudioClip Cassatt1;
-    public AudioClip[] Cassatt2;
-    public AudioClip[] Degas2;
+    //public AudioClip Pop;
+    //public AudioClip fxButton;
+    AudioSource _audioSource;
+    public GameObject musicObj;
 
-    private AudioSource _audioSource;
+    //public AudioMixerSnapshot defaultSnapshot;
+    //public AudioMixerSnapshot tunelSnapshot;
+    //public AudioMixerSnapshot submarinoSnapshot;
 
-    private Dictionary<string, AudioClip[]> Sounds = new Dictionary<string, AudioClip[]>();
+    AudioSource audioMusic;
+    
 
-    public static AudioManager Instance { get; private set; }
+    public static AudioManager Instance;
 
-    void Awake()
-    {
-        if (Instance != null)
-        {
-            Destroy(gameObject);
-            return;
+    void Awake(){
+
+        if(Instance != null && Instance != this){
+            Destroy(this.gameObject);
+        }else{
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
         }
 
-        Instance = this;
-        DontDestroyOnLoad(gameObject);
-        _audioSource = GetComponent<AudioSource>();
-
-        SceneManager.sceneLoaded += OnSceneLoaded;
-
-        // Añadir los clips de audio a las escenas en el diccionario
-        Sounds.Add("Menu", new AudioClip[] { MenuEscena1 });
-        Sounds.Add("Escena1", new AudioClip[] { MenuEscena1 });
-        Sounds.Add("Escena2", Escena2);
-        Sounds.Add("Escena3", new AudioClip[] { Degas1 });
-        Sounds.Add("Degas1", new AudioClip[] { Degas1 });
-        //Sounds.Add("Degas2", new AudioClip [] { Degas2 });
-        Sounds.Add("Cassatt1", new AudioClip [] { Cassatt1 });
-        //Sounds.Add("Cassatt2", new AudioClip [] { Cassatt2 });
         
     }
 
-    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    // Start is called before the first frame update
+    void Start()
     {
-        string sceneName = scene.name;
-        if (!Sounds.ContainsKey(sceneName))
-        {
-            StopSound();
-            return;
-        }
+        _audioSource = GetComponent<AudioSource>();
+        
+        audioMusic = musicObj.GetComponent<AudioSource>();
+        audioMusic = this.GetComponent<AudioSource>();
+        //audioMusic.clip = pop;
+        audioMusic.loop = true;
+        audioMusic.volume = 0.2f;
+        audioMusic.Play();
 
-        AudioClip[] sceneSounds = Sounds[sceneName];
-        if (sceneSounds.Length == 0)
-        {
-            StopSound();
-            return;
-        }
-
-        AudioClip randomSound = sceneSounds[Random.Range(0, sceneSounds.Length)];
-        PlaySound(randomSound);
+        
     }
 
-    private void PlaySound(AudioClip clip)
+    // Update is called once per frame
+    void Update()
     {
-        if (!_audioSource.isPlaying)
-        {
-            _audioSource.clip = clip;
-            _audioSource.loop = true;
-            _audioSource.Play();
-        }
+        
     }
 
-    private void StopSound()
-    {
-        _audioSource.Stop();
+//metodo para hacer sonar clips de audio
+    public void SonarClipUnaVez(AudioClip ac ){
+        _audioSource.PlayOneShot(ac);
     }
 
-    private void OnDestroy()
-    {
-        SceneManager.sceneLoaded -= OnSceneLoaded;
+    /*public void IniciarEfectoTunel(){
+        tunelSnapshot.TransitionTo(0.5f);
     }
-}
+
+    public void IniciarEfectoBurbuja(){
+        submarinoSnapshot.TransitionTo(1f);
+    }
+
+    public void IniciarEfectoDefault(){
+        defaultSnapshot.TransitionTo(0.5f);
+    }
+}*/
